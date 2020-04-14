774bf3b559d012f3b80218170720774ee84bd06f
j-lapp <jarodlapp@gmail.com>
2020-04-15 00:51:40 +0300

    parks tooltip and hover highlight

--- src/components/geojsonmap.js
+++ src/components/geojsonmap.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map as LeafletMap, GeoJSON, Marker, Popup } from 'react-leaflet';
+import { Map as LeafletMap, GeoJSON, Marker, Popup, Tooltip } from 'react-leaflet';
 import jarod from '../data/j.jpg';
 import L from 'leaflet';
 import bcGeoJSON from '../data/BC.json';
@@ -7,24 +7,38 @@ import bcParks from '../data/BCparks.json';
 
 class GeoJsonMap extends React.Component {
   render() {
-	 const iconPerson = new L.Icon({
-        iconUrl: jarod,
-        // iconRetinaUrl: require('../img/marker-pin-person.svg'),
-        // iconAnchor: null,
-        // popupAnchor: null,
-        // shadowUrl: null,
-        // shadowSize: null,
-        // shadowAnchor: null,
-        iconSize: new L.Point(50, 50),
-        className: 'leaflet-div-icon'
-    });
+    const onEachPark = (feature, layer) => {
+      const TooltipContent = `<b><span style="font-size: 16px; color: #14a05a">${feature.properties.Pa_name}</span></b><br>
+      <span style="font-size: 13px; color: #383838">Provincial Park</span>`;
+      
+      layer.bindTooltip(TooltipContent);
+      
+      layer.on('mouseover', function () {
+        this.setStyle({
+          'weight': '0.6',
+          'color': '#23bb81',
+          'fillColor': '#bbfedd'
+        });
+      });
+      
+      layer.on('mouseout', function () {
+        this.setStyle({
+          'fillColor': '#8dd587',
+          'color': '#066200',
+          'weight': '0.2'
+        });
+      });
+    };
+
     return (
       <LeafletMap
-        center={[49.5, -124]}
+        center={[49.6, -124.3]}
         zoom={8}
-        maxZoom={10}
+        zoomDelta = {0.01}
+        zoomSnap = {0.01}
+        maxZoom = {13}
         attributionControl={true}
-        zoomControl={true}
+        zoomControl={false}
         doubleClickZoom={true}
         scrollWheelZoom={true}
         dragging={true}
@@ -42,20 +56,22 @@ class GeoJsonMap extends React.Component {
         />
 		<GeoJSON
           data={bcParks}
+          onEachFeature = {onEachPark}
           style={() => ({
             color: '#066200',
             weight: 0.2,
             fillColor: "#8dd587",
             fillOpacity: 0.8,
-          })}
-		  
-        />
-        <Marker position={[48.37, -123.38]}
-		>
+          })} >
+		</GeoJSON>
+{/* 		<Marker 
+            position={[48.37, -123.38]}
+            icon={iconPerson}
+        >
           <Popup>
             Popup for any custom information.
           </Popup>
-        </Marker>
+        </Marker> */}
       </LeafletMap>
     );
   }
